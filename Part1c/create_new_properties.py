15d36d4555b882a76a4afec2d2e43d3006c5a81c
lorenzopensource <lorenzo.pasqualotto01@gmail.com>
2025-09-23 14:05:40 +0200

    Part1c - Generating random new properties

---
+++ Part1c/create_new_properties.py
@@ -0,0 +1,27 @@
+import csv
+import random
+
+with open('restaurant_info.csv','r') as csvinput:
+    with open('restaurant_info_new_properties.csv', 'w') as csvoutput:
+        writer = csv.writer(csvoutput, lineterminator='\n')
+        reader = csv.reader(csvinput)
+
+        all = []
+        row = next(reader)
+        row.append("foodquality")
+        row.append("crowdness")
+        row.append("lengthofstay")
+        all.append(row)
+
+        for row in reader:
+            row.append(random.choice(["good", "bad"]))
+            row.append(random.choice(["busy", "not_busy"]))
+            row.append(random.choice(["short", "long"]))
+            all.append(row)
+
+        writer.writerows(all)
+
+
+
+
+
